6d20f34b57d27c3665a4e7e7eaf9d490a6c1943d
AshenPheonix <brendlorend@gmail.com>
2020-01-09 15:21:05 -0800

    wip: some modeling

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="container">
+        <section class="name">
+            <p>
+                {{name}}
+            </p>
+        </section>
+        <section class="email">
+            <p>
+                {{email}}
+            </p>
+        </section>
+    </section>
+</template>
+
+<script>
+    export default {
+        props:{
+            name:[String],
+            email:[String]
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Single.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="container" v-if="user.name!=undefined">
+        <section class="name">
+            {{user.name}}
+        </section>
+        <section class="email">
+            {{user.email}}
+        </section>
+    </section>
+    <section class="container" v-else="">
+        Loading...
+    </section>
+</template>
+
+<script>
+    import {mapState} from 'vuex'
+    export default {
+        computed:mapState({
+            user:state=>state.user,
+            userAlias:'user'
+        }),
+        mounted:function(){
+            this.$store.dispatch('getPeople')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
